4de5c084d2b25c9ed81ae2178ad916da82c625ce
Ewan <vulcanv6@hotmail.com>
2018-01-12 10:42:00 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,27 +3,25 @@ import discord
 import asyncio
 import json
 from discord.ext import commands
-from config import *
 
-
-#with open('config.json') as json_data_file:
-    #data = json.load(json_data_file)
-
+try:
+    
+    with open('config.json') as json_data_file:
+        data = json.load(json_data_file)
+        print('Config Loaded!')
+except Exception as e:
+    print('There was an error: {}'.format(e))
 
 bot = commands.Bot(command_prefix=prefix, description = description)
 ext_commands = ["main"]
-client = discord.Client()
-
-
 
 
 @bot.event
 async def on_ready():
-    #with open('config.json') as json_data_file:
-        #data = json.load(json_data_file)
+    
 
     print("Bot Logged in!")
-    owner = await bot.get_user_info('211098424655740929')
+    owner = await bot.get_user_info(config['oner_id'])
     await bot.send_message(owner, "Bot has came online!")
     await bot.change_presence(game=(discord.Game(name=status)))
     for i in range(len(ext_commands)+1):
@@ -34,4 +32,4 @@ async def on_ready():
 def load_cogs(bot):
     defaults = ("main")
 
-bot.run(token)
+bot.run(config['token'])
